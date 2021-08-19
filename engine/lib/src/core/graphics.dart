14162d45c2cb69fe7fbb0cf153bed63e9239d730
Erick Zanardo <erickzanardoo@gmail.com>
2021-08-19 18:40:41 -0300

    adding color palette

--- engine/lib/src/core/graphics.dart
+++ engine/lib/src/core/graphics.dart
@@ -5,7 +5,7 @@ class EmberPalette {
 
   const EmberPalette(this.colors);
 
-// palette: https://lospec.com/palette-list/ammo-8
+  // palette: https://lospec.com/palette-list/ammo-8
   const EmberPalette.base()
       : this(const [
           Color(0XFF040C06),
@@ -16,6 +16,19 @@ class EmberPalette {
           Color(0XFF89A257),
           Color(0XFFBEDC7F),
           Color(0XFFEEFFCC),
+        ]);
+
+  // palette: https://lospec.com/palette-list/endesga-8
+  const EmberPalette.color()
+      : this(const [
+          Color(0XFF1B1C33),
+          Color(0XFF7B53AD),
+          Color(0XFF2D93DD),
+          Color(0XFF28C641),
+          Color(0XFFE6DA29),
+          Color(0XFFDA7D22),
+          Color(0XFFD32734),
+          Color(0XFFFDFDF8),
         ]);
 
   Color get backgroundColor => colors.first;
