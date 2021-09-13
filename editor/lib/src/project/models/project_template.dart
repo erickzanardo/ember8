d712d2aa6fe860e2f751a464bf11fbc84d5ce2ba
Erick Zanardo <erickzanardoo@gmail.com>
2021-09-13 19:45:06 -0300

    adding project template serialization and deserialization

--- editor/lib/src/project/models/project_template.dart
+++ editor/lib/src/project/models/project_template.dart
@@ -8,6 +8,38 @@ class ProjectTemplateField<T> extends Equatable {
 
   @override
   List<Object?> get props => [name, value];
+
+  static ProjectTemplateField fromData(List data) {
+    final name = data[0] as String;
+    final value = data[1];
+    final type = data[2] as String;
+
+    if (type == 'S') {
+      return ProjectTemplateField<String>(name, value as String);
+    } else if (type == 'D') {
+      return ProjectTemplateField<double>(name, value as double);
+    } else if (type == 'B') {
+      return ProjectTemplateField<bool>(name, value as bool);
+    } else {
+      throw ArgumentError('Unknown type: $type');
+    }
+  }
+
+  List<dynamic> toData() {
+    late String type;
+
+    if (T == String) {
+      type = 'S';
+    } else if (T == double) {
+      type = 'D';
+    } else if (T == bool) {
+      type = 'B';
+    } else {
+      throw ArgumentError('Unknown type: $T');
+    }
+
+    return [name, value, type];
+  }
 }
 
 class ProjectTemplate extends Equatable {
@@ -27,5 +59,25 @@ class ProjectTemplate extends Equatable {
         field,
       ],
     );
+  }
+
+  static ProjectTemplate fromData(List<dynamic> data) {
+    final name = data[0] as String;
+
+    final fieldsData = data[1] as List<List<dynamic>>;
+    final fields = fieldsData
+        .map(
+          (data) => ProjectTemplateField.fromData(data),
+        )
+        .toList();
+
+    return ProjectTemplate(name, fields);
+  }
+
+  List<dynamic> toData() {
+    return [
+      name,
+      fields.map((field) => field.toData()).toList(),
+    ];
   }
 }
---
+++ editor/test/project/models/project_template_test.dart
@@ -0,0 +1,69 @@
+import 'package:editor/src/project/models/project.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Project - models - Project Template Field', () {
+    group('double fields', () {
+      test('correctly serializes', () {
+        const field = ProjectTemplateField<double>('field', 0.2);
+        expect(field.toData(), equals(['field', 0.2, 'D']));
+      });
+      test('correctly deserializes', () {
+        const data = ['field', 0.2, 'D'];
+        const field = ProjectTemplateField<double>('field', 0.2);
+        final generatedField = ProjectTemplateField.fromData(data);
+
+        expect(generatedField, equals(field));
+        expect(generatedField, isA<ProjectTemplateField<double>>());
+      });
+    });
+    group('bool fields', () {
+      test('correctly serializes', () {
+        const field = ProjectTemplateField<bool>('field', true);
+        expect(field.toData(), equals(['field', true, 'B']));
+      });
+      test('correctly deserializes', () {
+        const data = ['field', true, 'B'];
+        const field = ProjectTemplateField<bool>('field', true);
+        final generatedField = ProjectTemplateField.fromData(data);
+
+        expect(generatedField, equals(field));
+        expect(generatedField, isA<ProjectTemplateField<bool>>());
+      });
+    });
+    group('String fields', () {
+      test('correctly serializes', () {
+        const field = ProjectTemplateField<String>('field', 'value');
+        expect(field.toData(), equals(['field', 'value', 'S']));
+      });
+      test('correctly deserializes', () {
+        const data = ['field', 'value', 'S'];
+        const field = ProjectTemplateField<String>('field', 'value');
+        final generatedField = ProjectTemplateField.fromData(data);
+
+        expect(generatedField, equals(field));
+        expect(generatedField, isA<ProjectTemplateField<String>>());
+      });
+    });
+  });
+  group('Project - models - Project Template', () {
+    test('correctly serializes', () {
+      const field = ProjectTemplateField<String>('field', 'value');
+      const template = ProjectTemplate('template', [field]);
+      expect(template.toData(), equals([
+        'template',
+        [field.toData()],
+      ]));
+    });
+    test('correctly deserializes', () {
+      const field = ProjectTemplateField<String>('field', 'value');
+      final data = [
+        'template',
+        [field.toData()],
+      ];
+      const template = ProjectTemplate('template', [field]);
+
+      expect(ProjectTemplate.fromData(data), equals(template));
+    });
+  });
+}
